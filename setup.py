c39c4a5e756c68bc842ea1bb5242f70daddfac00
Juliya Herasymniuk <juliya.gerasimnjyk@gmail.com>
2022-09-15 16:04:30 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='clean',
+      version='1',
+      description='Useful code to clean and sort your folders',
+      url='https://github.com/YuliyaGerasymniuk/sort-files.py.git',
+      author='Yuliya Herasymniuk',
+      author_email='juliya.gerasimnjyk@gmail.com',
+      license='MIT',
+      packages=["clean_folder"]
+      )
